82645acb0cb747c4b1d1c1816184042cf978dc4f
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2021-03-04 10:46:24 -0100

    Experience section complete

--- client/src/components/Experience.js
+++ client/src/components/Experience.js
@@ -10,48 +10,66 @@ const Experience = () => {
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h4>2014-2015</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, maiores enim mollitia voluptatibus minus quisquam reiciendis quas laboriosam asperiores quo eum molestiae non temporibus rerum cupiditate dolorum culpa minima nemo tenetur, hic eveniet soluta perspiciatis commodi doloribus? Natus, nesciunt voluptatem.</p>
+                        <h4>2020-Present</h4>
+                        <h5>Tutor for Full Stack Coding Program at 2U</h5>
+                        <p>.Supports Students during their program. Manages personal roster and sessions</p>
+                        <p>.Manages personal roster and sessions</p>
+                        <p>.Researchs and continues learning to give students the best support</p>
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h4>2014-2015</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, maiores enim mollitia voluptatibus minus quisquam reiciendis quas laboriosam asperiores quo eum molestiae non temporibus rerum cupiditate dolorum culpa minima nemo tenetur, hic eveniet soluta perspiciatis commodi doloribus? Natus, nesciunt voluptatem.</p>
+                        <h4>2019-2020</h4>
+                        <h5>Case Manager at Lutheran Social Services</h5>
+                        <p>Worked in temporary asylum shelters all over the Phoenix valley to deliver information to asylum seekers about their rights.</p>
+                        <p>Did Know Your Rights seminars in the shelter in San Luis, Mexico for asylum seekers who had not yet crossed the border.</p>
+                        <p>Created the contents in the Know your Rights package offered to every asylum seeker that I consulted in the shelters.</p>
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h4>2015-2016</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, maiores enim mollitia voluptatibus minus quisquam reiciendis quas laboriosam asperiores quo eum molestiae non temporibus rerum cupiditate dolorum culpa minima nemo tenetur, hic eveniet soluta perspiciatis commodi doloribus? Natus, nesciunt voluptatem.</p>
+                        <h4>2016-2020</h4>
+                        <h5>Producer at Inclusive Activism</h5>
+                        <p>.Supported script writing/editing for diversity inclusion podcast</p>
+                        <p>.Edited audio content</p>
+                        <p>.Produced final podcast for publication</p>
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h4>2016-2017</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, maiores enim mollitia voluptatibus minus quisquam reiciendis quas laboriosam asperiores quo eum molestiae non temporibus rerum cupiditate dolorum culpa minima nemo tenetur, hic eveniet soluta perspiciatis commodi doloribus? Natus, nesciunt voluptatem.</p>
+                        <h4>2018-2019</h4>
+                        <h5>Comunication Specialist at Next Generation Service Corps</h5>
+                        <p>Created the informational website used to communicate with students.</p>
+                        <p>Maintained and managed content of the website.</p>
+                        <p>Created and managed the bi-weekly newsletter sent to all the students in the program.</p>
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h4>2017-2018</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, maiores enim mollitia voluptatibus minus quisquam reiciendis quas laboriosam asperiores quo eum molestiae non temporibus rerum cupiditate dolorum culpa minima nemo tenetur, hic eveniet soluta perspiciatis commodi doloribus? Natus, nesciunt voluptatem.</p>
+                        <h4>2009-2009</h4>
+                        <h5>Production Assistant Intern at Horizon Pictures</h5>
+                        <p>.Managed content exposure to public and professional contests</p>
+                        <p>.Solicited corp. to corp. relationships to arrange new production agreements</p>
+                        <p>.Worked directly with editing team to ensure productions remained on schedule</p>
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h4>2018-2020</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, maiores enim mollitia voluptatibus minus quisquam reiciendis quas laboriosam asperiores quo eum molestiae non temporibus rerum cupiditate dolorum culpa minima nemo tenetur, hic eveniet soluta perspiciatis commodi doloribus? Natus, nesciunt voluptatem.</p>
+                        <h4>2005-2007</h4>
+                        <h5>Assistant Producer at L'Appart Bureau</h5>
+                        <p>Prepared models and sets for photo shoots.</p>
+                        <p>Organized runway fashion shows.</p>
+                        <p>Coordinated media content and exposure for Paris based fashion design agency.</p>
                     </div>
                 </div>
             </div>
